ba3afbdd5c6a598e11e824ed5a157018c98263e5
StockAcademy <medikurthi.jagadish.mgm@gmail.com>
2026-04-21 10:48:01 -0700

    Add files via upload

---
+++ stock-academy-pwa/stock-academy-pwa/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'stock-academy-v2';
+const ASSETS = ['./index.html'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
